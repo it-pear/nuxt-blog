ee42fbc86c5caf042b961267a6d00c7564290808
yury blinov <yurecblinovgelarm@gmail.com>
2021-02-19 15:14:58 +0300

    add server setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,15 @@
-export default {
+const pkg = require('./package')
+
+
+module.exports = {
+  mode: 'universal',
+
   head: {
-    title: 'nuxt-blog',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -25,18 +27,16 @@ export default {
     '@/plugins/globals'
   ],
 
-  components: true,
-
-  buildModules: [
-  ],
-
   modules: [
-    '@nuxtjs/axios',
+    '@nuxtjs/axios'
   ],
 
   axios: {},
 
   build: {
     transpile: [/^element-ui/],
+    extend(config, ctx) {
+
+    }
   }
 }
---
+++ server/keys/keys.dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGO_URI: `mongodb+srv://admin:Rootgel2000@cluster0.3npwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+}
